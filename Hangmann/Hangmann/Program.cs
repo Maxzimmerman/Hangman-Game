aa9602c2fb521ffe29ffc8b04833f9fa4d249708
Maxzimmerman <115488694+Maxzimmerman@users.noreply.github.com>
2023-02-08 16:46:55 +0100

    Add files via upload

---
+++ Hangmann/Hangmann/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangmann
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu();
+        }
+
+        static void MainMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("### HANGMAN ###");
+                Console.WriteLine("###############");
+                Console.WriteLine(" ");
+
+
+                Console.WriteLine("Wähle eine Aktion aus");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("[1] Spielen");
+                Console.WriteLine("[2] Beenden");
+                Console.ResetColor();
+                Console.WriteLine(" ");
+
+                Console.Write("Aktion:");
+                int action = Convert.ToInt32(Console.ReadLine());
+                bool end = false;
+
+                switch (action)
+                {
+                    case 1:
+                        StartGame();
+                        break;
+
+                    case 2:
+                        end = true;
+                        break;
+                }
+
+                if (end)
+                {
+                    break;
+                }
+                Console.Clear();
+            }
+        }
+
+        static void StartGame()
+        {
+            string[] words = new string[]
+            {
+                "Apfelkuchen",
+                "Gemüseesuppe",
+                "Kraftfahrzeug",
+                "Lastwagen",
+                "Videospiel",
+                "Alarmanlage",
+                "Vollkornbrot"
+            };
+
+            Random rnd = new Random();
+            int index = rnd.Next(0, words.Length);
+            string word = words[index].ToLower();
+            GameLoop(word);
+        }
+
+        static void GameLoop(string word)
+        {
+            int lives = 10;
+            string hiddenWord = "";
+
+            for(int i = 0; i < word.Length; i++)
+            {
+                hiddenWord += "_";
+            }
+
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine(word);
+                Console.WriteLine("Gesuchtes Wort: {0} ", hiddenWord);
+                Console.Write("Noch ürige Versuche:");
+
+
+                for(int i =0; i < lives; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("X");
+                    Console.ResetColor();
+
+                }
+
+                Console.WriteLine();
+                Console.Write("Buchstabe: ");
+                char character = Convert.ToChar(Console.ReadLine().ToLower());
+
+                bool foundCharacterInWord = false;
+                
+                for(int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == character)
+                    {
+                        foundCharacterInWord = true;
+                        break;
+                    }
+                }
+
+                string tempHiddenWord = hiddenWord;
+                hiddenWord = "";
+
+                if (foundCharacterInWord)
+                {
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (word[i] == character)
+                        {
+                            hiddenWord += character;
+                        }
+                        else if (tempHiddenWord[i] != '_')
+                        {
+                            hiddenWord += tempHiddenWord[i];
+                        }
+                        else
+                        {
+                            hiddenWord += '_';
+                        }
+                    }
+
+                    if (hiddenWord == word)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("GEWONNEN");
+                        Console.WriteLine("Das Wort war: {0} ", word);
+                        Console.ReadKey();
+                        break;
+                    }
+                }
+
+                else
+                {
+                    hiddenWord = tempHiddenWord;
+
+                    if(lives > 0)
+                    {
+                        lives -= 1;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("GAME OVER!");
+                        Console.ReadKey();
+                        Console.ResetColor();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
